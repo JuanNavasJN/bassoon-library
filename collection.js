c038069a4946feaea80846650612b7d1bbbe4ae7
Juan Navas <navascruzf@gmail.com>
2018-07-25 10:24:23 -0400

    initial commit

---
+++ collection.js
@@ -0,0 +1,205 @@
+export const collection = [
+    // {
+    //     "title": "",
+    //     "composer": "",
+    //     "url": ""
+    // },
+    {
+        "title": "Sonata for bassoon and klavier f-moll",
+        "composer": "Georg Philipp Telemann",
+        "url": "http://fagotizm.karsav.com/sheet_music/telemann_sonata.pdf"
+    },
+    {
+        "title": "Concerto for bassoon and orchestra (piano)",
+        "composer": "A. Tomazi",
+        "url": "http://fagotizm.karsav.com/sheet_music/tomazi-klavir.pdf"
+    },
+    {
+        "title": "Concerto for bassoon and orchestra (bassoon)",
+        "composer": "A. Tomazi",
+        "url": "http://fagotizm.karsav.com/sheet_music/tomazi-fagot.pdf"
+    },
+    {
+        "title": "Concerto for bassoon and orchestra F-Dur (bassoon and piano parts)",
+        "composer": "Franz Danzi",
+        "url": "http://fagotizm.karsav.com/sheet_music/danzi-f-dur.pdf"
+    },
+    {
+        "title": "Partita for Bassoon solo (BWV 1013)",
+        "composer": "I. S. Bach",
+        "url": "http://fagotizm.karsav.com/sheet_music/bach_partita_for_bassoon_solo.pdf"
+    },
+    {
+        "title": "Variations and Rondo for Bassoon and orchestra Op. 57 (bassoon and piano part)",
+        "composer": "Johann Kalliwoda",
+        "url": "http://fagotizm.karsav.com/sheet_music/kalliwoda_Variations_for_bassoon_&_orchestra_in_Bb_Op.57.pdf"
+    },
+    {
+        "title": "Andante and Hungarian rondo for bassoon and orchestra (piano)",
+        "composer": "C. M. Weber",
+        "url": "http://fagotizm.karsav.com/sheet_music/Weber_Andante_e_Rondo_ungarese-klavier.pdf"
+    },
+    {
+        "title": "Andante and Hungarian rondo for bassoon and orchestra (bassoon)",
+        "composer": "C. M. Weber",
+        "url": "http://fagotizm.karsav.com/sheet_music/weber-fantasia-fagot.pdf"
+    },
+    {
+        "title": "Concerto for bassoon and orchestra F-dur (klavier and bassoon part)",
+        "composer": "C. M. Weber",
+        "url": "http://fagotizm.karsav.com/sheet_music/weber-concerto-F-Dur.pdf"
+    },
+    {
+        "title": "Sonata for bassoon and piano e-moll (bassoon and piano parts)",
+        "composer": "Benedetto Marcello",
+        "url": "http://fagotizm.karsav.com/sheet_music/marcello.zip"
+    },
+    {
+        "title": "Concerto for bassoon and orchestra (bassoon part)",
+        "composer": "Hummel",
+        "url": "http://fagotizm.karsav.com/sheet_music/humel.zip"
+    },
+    {
+        "title": "Concerto for bassoon and orchestra B-dur K. 191 (midi)",
+        "composer": "W. A. Mozart",
+        "url": "http://fagotizm.karsav.com/sheet_music/mozart_Concerto_B-dur.mid"
+    },
+    {
+        "title": "Concerto for bassoon and orchestra B-dur K. 191 (score)",
+        "composer": "W. A. Mozart",
+        "url": "http://fagotizm.karsav.com/sheet_music/mozart-score.pdf"
+    },
+    {
+        "title": "Concerto for bassoon and orchestra B-dur K. 191 (piano)",
+        "composer": "W. A. Mozart",
+        "url": "http://fagotizm.karsav.com/sheet_music/mozart-klavir.zip"
+    },
+    {
+        "title": "Concerto for bassoon and orchestra B-dur K. 191 (bassoon)",
+        "composer": "W. A. Mozart",
+        "url": "http://fagotizm.karsav.com/sheet_music/mozart-fagot.zip"
+    },
+    {
+        "title": "Concertino for bassoon and orchestra (bassoon and piano parts)",
+        "composer": "Marcel Bitsch",
+        "url": "http://fagotizm.karsav.com/sheet_music/bitsch-concertino_pour_basson_et_orchestra.pdf"
+    },
+    {
+        "title": "Concerto for bassoon and orchestra (bassoon part)",
+        "composer": "A. Joulivet",
+        "url": "http://fagotizm.karsav.com/sheet_music/joulivet-fagot.zip"
+    },
+    {
+        "title": "Sarabande et Cortege for bassoon and piano (bassoon and piano part)",
+        "composer": "Henri Dutilleux",
+        "url": "http://fagotizm.karsav.com/sheet_music/dutilleux-sarabande-cortege.pdf"
+    },
+    {
+        "title": "Suite for bassoon and piano (bassoon part and piano part)",
+        "composer": "Longo",
+        "url": "http://fagotizm.karsav.com/sheet_music/Longo_Suite_op.69.pdf"
+    },
+    {
+        "title": "Sonata for bassoon and piano (bassoon part and piano part)",
+        "composer": "K. Saint-Sans",
+        "url": "http://fagotizm.karsav.com/sheet_music/ssans-sonate.pdf"
+    },
+    {
+        "title": "Concerto No. 2 for bassoon and piano (piano)",
+        "composer": "L. Milde",
+        "url": "http://fagotizm.karsav.com/sheet_music/milde-klavir.zip"
+    },
+    {
+        "title": "Concerto No. 2 for bassoon and piano (bassoon)",
+        "composer": "L. Milde",
+        "url": "http://fagotizm.karsav.com/sheet_music/milde-fagot.zip"
+    },
+    {
+        "title": "Concerto F-Dur for 2 bassoons and orchestra (bassoon I, II and piano parts)",
+        "composer": "J. B. Wanhal",
+        "url": "http://fagotizm.karsav.com/sheet_music/ensamble/Wanhal-2-Bassoons-concerto.pdf"
+    },
+    {
+        "title": "Bassoon solo from 'Bolero'",
+        "composer": "M. Ravel",
+        "url": "http://fagotizm.karsav.com/sheet_music/bolero.zip"
+    },
+    {
+        "title": "Bassoon solo from 7 symphony ('Leningrad')",
+        "composer": "D. Shostakovich",
+        "url": "http://fagotizm.karsav.com/sheet_music/shostakovich7-bassoon-solo.pdf"
+    },
+    {
+        "title": "Escolaso from 'Suite Troileana' for bassoon and strings (score and all parts)",
+        "composer": "A. Piazzola",
+        "url": "http://fagotizm.karsav.com/sheet_music/A.Piazzolla-Escolaso-fagotto&archi.zip"
+    },
+    {
+        "title": "Sonata for bassoon and piano (bassoon and piano parts)",
+        "composer": "Paul Hindemith",
+        "url": "http://fagotizm.karsav.com/sheet_music/hindemit.pdf"
+    },
+    {
+        "title": "Concerto for bassoon and orchestra (bassoon and piano parts)",
+        "composer": "B. Boismortier",
+        "url": "http://fagotizm.karsav.com/sheet_music/boismortier-concerto.pdf"
+    },
+    {
+        "title": "Concerto (bassoon part)",
+        "composer": "Jean Francaix",
+        "url": "http://fagotizm.karsav.com/sheet_music/francaix.zip"
+    },
+    {
+        "title": "Partita for Bassoon solo",
+        "composer": "Gordon Jacob",
+        "url": "http://fagotizm.karsav.com/sheet_music/gordon-partita.pdf"
+    },
+    {
+        "title": "Recit, Cicilienne et Rondo for bassoon and piano (bassoon and piano parts)",
+        "composer": "E. Bozza",
+        "url": "http://fagotizm.karsav.com/sheet_music/bozza_rec-cic-rondo.pdf"
+    },
+    {
+        "title": "Fantasia for bassoon and piano (bassoon and piano parts)",
+        "composer": "E. Bozza",
+        "url": "http://fagotizm.karsav.com/sheet_music/bozza.zip"
+    },
+    {
+        "title": "Sonatine for bassoon and piano (bassoon and piano parts)",
+        "composer": "A. Tansman",
+        "url": "http://fagotizm.karsav.com/sheet_music/tansman-sonatine.zip"
+    },
+    {
+        "title": "Suite for bassoon and piano (bassoon and piano parts)",
+        "composer": "A. Tansman",
+        "url": "http://fagotizm.karsav.com/sheet_music/tansman-suite.zip"
+    },
+    {
+        "title": "Variations on thema of Paganini for solo Bassoon",
+        "composer": "Maurice Allard",
+        "url": "http://fagotizm.karsav.com/sheet_music/paganini-allard-bassoon-solo-variations.pdf"
+    },
+    {
+        "title": "Solo de Concert (bassoon and piano parts)",
+        "composer": "G. Pierne",
+        "url": "http://fagotizm.karsav.com/sheet_music/pierne.pdf"
+    },
+    {
+        "title": "Concertos for bassoon Vol. 1 (concertos d-moll, e-moll, a-moll, C-Dur, c-moll)",
+        "composer": "A. Vivaldi",
+        "url": "http://fagotizm.karsav.com/sheet_music/vivaldi-schoenbach-bassoon-concerti-vol_1.rar"
+    },
+    {
+        "title": "Concerto for bassoon and orchestra a-moll F. VIII No. 2 (score and bassoon solo part)",
+        "composer": "A. Vivaldi",
+        "url": "http://fagotizm.karsav.com/sheet_music/vivaldi-a-moll-concert-score-fagotto-part.pdf"
+    },
+    {
+        "title": "Fantasy for Bassoon solo",
+        "composer": "Malcolm Arnold",
+        "url": "http://fagotizm.karsav.com/sheet_music/malcolm_arnold-fantasy_for_bassoon_solo.pdf"
+    }
+]
+
+export default {}
+    
